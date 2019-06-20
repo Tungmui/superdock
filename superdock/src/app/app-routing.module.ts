654c9a76372a41de166937bbcff5cb5d2bf8aed1
tx-gy <402061945@qq.com>
2019-06-20 11:39:17 +0800

    添加登录页面

---
+++ superdock/src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+
+const routes: Routes = [
+  { path: "", component: AppComponent },
+  { path: "login", component: LoginComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
